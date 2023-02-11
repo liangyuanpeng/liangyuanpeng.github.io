5224cae8e12835b3361ce43fab07f77b84ca830b
Lan Liang <gcslyp@gmail.com>
2023-02-11 15:54:09 +0800

    u

--- build.sh
+++ build.sh
@@ -1,12 +1,14 @@
 #/bin/sh
 
 cd content/files
-tar -czf k8s-admissionregistration-with-cel.tar.gz k8s-admissionregistration-with-cel
-ls
-pwd
+# tar -czf k8s-admissionregistration-with-cel.tar.gz k8s-admissionregistration-with-cel
 cd bin 
-wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_windows_amd64.zip
-wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_darwin_amd64.tar.gz
+# wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_windows_amd64.zip
+# wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_darwin_amd64.tar.gz
 wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_amd64.tar.gz
-wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_arm64.tar.gz
+# wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_arm64.tar.gz
+# wget --no-check-certificate https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_amd64.tar.gz
+ls
+tar -xf oras_0.16.0_linux_amd64.tar.gz
+rm LICENSE
 ls
