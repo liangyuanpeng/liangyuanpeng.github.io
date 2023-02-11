095483323e3aa438bda07e4e04f4d88c3496fe26
Lan Liang <gcslyp@gmail.com>
2023-02-11 15:36:45 +0800

    完善二进制文件oras下载托管

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #/bin/sh
 
-curl -X GET https://lank8s.cn/v3
+cd content/files
+tar -czf k8s-admissionregistration-with-cel.tar.gz k8s-admissionregistration-with-cel
+cd bin 
+wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_windows_amd64.zip
+wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_darwin_amd64.tar.gz
+wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_amd64.tar.gz
+wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_arm64.tar.gz
+ls
--- content/files/bin/README.md
+++ content/files/bin/README.md
@@ -2,3 +2,14 @@
 
 - kind   kind/xxx
 - oras   oras/xxx
+
+
+提供二进制文件下载
+
+- kind   kind/xxx
+- oras   oras/xxx
+
+https://liangyuanpeng.com/files/bin/oras_0.16.0_windows_amd64.zip
+https://liangyuanpeng.com/files/bin/oras_0.16.0_darwin_amd64.tar.gz
+https://liangyuanpeng.com/files/bin/oras_0.16.0_linux_amd64.tar.gz
+https://liangyuanpeng.com/files/bin/oras_0.16.0_linux_arm64.tar.gz
