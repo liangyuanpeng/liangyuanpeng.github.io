013da23a89723102091ace043a526a66ac71b4ec
Lan Liang <gcslyp@gmail.com>
2023-02-11 15:41:09 +0800

    添加修改静态文件README.md为index.yaml

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 cd content/files
 tar -czf k8s-admissionregistration-with-cel.tar.gz k8s-admissionregistration-with-cel
+ls
+pwd
 cd bin 
 wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_windows_amd64.zip
 wget https://github.worker.liangyuanpeng.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_darwin_amd64.tar.gz
