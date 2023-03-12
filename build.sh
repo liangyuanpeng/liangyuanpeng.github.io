486533aa759a500818e357af0a52b7d9cf22b8c1
Lan Liang <gcslyp@gmail.com>
2023-03-12 12:57:23 +0800

    发布一篇文章介绍使用 Kind 搭建 kubernetes 环境

--- build.sh
+++ build.sh
@@ -12,6 +12,16 @@ cd bin
 # tar -xf oras_0.16.0_linux_amd64.tar.gz
 # rm LICENSE
 
+wget https://github.com/kubernetes-sigs/kind/releases/download/v0.17.0/kind-linux-amd64
+mv kind-linux-amd64 kind-0.17
+chmod +x kind-0.17
+
+wget https://github.com/oras-project/oras/releases/download/v0.16.0/oras_0.16.0_linux_amd64.tar.gz
+tar -xf oras_0.16.0_linux_amd64.tar.gz
+rm -f oras_0.16.0_linux_amd64.tar.gz
+rm -f LICENSE
+./oras
+
 # https://github.com/kubernetes-sigs/krew/releases/download/v0.4.3/krew-linux_amd64.tar.gz
 # https://github.com/kubernetes-sigs/krew/releases/download/v0.4.3/krew-darwin_arm64.tar.gz
 ls
