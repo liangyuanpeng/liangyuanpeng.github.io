842bd75306931c02add6c628821790318e64ad65
Lan Liang <gcslyp@gmail.com>
2023-04-11 15:26:33 +0800

    * 为utterances-bot创建的issue添加label
    * 当issue打开或编辑时，校验issue名称，拒绝非法issue名称

--- scripts/issueci.sh
+++ scripts/issueci.sh
@@ -1,9 +1,53 @@
-#/bin/sh
+#!/bin/bash
 
-echo "==========================="
-env
-echo "==========================="
+#echo "==========================="
+#env
+#echo "==========================="
 
-if [ $1 ];then
-	echo "issueci params: \n $1"
-else
+# issuenumber author title body type(opened|edited)
+
+function labelIssue(){
+  # params:
+  # issuenumber labels
+  # "comments,utterances"
+  gh issue edit $1 -R liangyuanpeng/liangyuanpeng.github.io --add-label $2
+}
+
+function commentIssue(){
+  gh issue comment $1 --body "如果本文对你很有用欢迎对本文评论,
+  ---
+  如果你有不一样的使用场景也欢迎投稿更新到本文当中来.
+  ---
+  如果你不方便评论也可以对本评论:+1:+1
+  ---"
+}
+
+echo "issue title:"$3
+echo "type:"$5
+
+issuetitletmp=$3
+issuetitle=`echo $issuetitletmp | sed 's/ *$//'`
+
+subTitle="liangyuanpeng's Blog"
+
+length=${#issuetitle}
+
+if [ $length -gt 20 ]; then  # 长度大于 10 小于 20
+  title_last=${issuetitle: -20}
+  if [ "$title_last" == "$subTitle" ]; then
+      if [ "$2" != "liangyuanpeng" -a "$2" != "utterances-bot" ];then
+        echo "valid title:"$issuetitle
+        newtitle=$issuetitle"-valid title"
+        echo $newtitle
+        gh issue edit $1 --title "$newtitle" -R liangyuanpeng/liangyuanpeng.github.io
+      fi
+  fi
+fi
+
+if [ "$2" == "utterances-bot" ];then
+  if [ "$5" == "opened" ];then
+    labelIssue $1 "comments,utterances"
+#    commentIssue $1
+  fi
+fi
+
